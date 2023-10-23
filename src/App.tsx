de64821c1c2674f47ee4fea5b335ffcaaf41e0e7
Daniel Falkenbert <daniel.falkenbert@aller.com>
2023-10-23 22:17:12 +0200

    Cleanup and removal of Prop-types for journey and stop data to minimise set states.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Toplist } from './Toplist';
-
-
-type JourneyProps = {
-  LineNumber: string,
-  DirectionCode: string,
-  JourneyPatternPointNumber: string,
-  LastModifiedUtcDateTime: string,
-  ExistsFromDate: string
-};
-
-type StopsProps = {
-  StopPointNumber: string,
-  StopPointName: string, 
-  StopAreaNumber: string,
-  LocationNorthingCoordinate: string,
-  LocationEastingCoordinate: string,
-  ZoneShortName: string,
-  StopAreaTypeCode: string,
-  LastModifiedUtcDateTime: string,
-  ExistsFromDate: string
-};
 
 type topProps = {
   count: number,
+  line: number,
   stops: any
-}
-
-function getKeysWithHighestValue(o:any, n:number){
-  var keys = Object.keys(o);
-  keys.sort(function(a,b){
-    return o[b] - o[a];
-  })
-  console.log(keys);
-  return keys.slice(0,n);
 }
 
 function App() {
   // Settings data from API
-  const [JourneyData, setJourneyData] = useState<JourneyProps[]>([]);
-  // const [StopsData, setStopsData] = useState<StopsProps[]>([]);
-
   const [top10Lines, settop10Lines] = useState<topProps[]>([]);
 
   useEffect(() => {
-    const JourneyApi = async () => {
-      
-      // Getting CORS errors from trafiklab. Mocking instead.
-      /*const data = await fetch("https://api.sl.se/api2/LineData.json?model=jour&key=b138ca24481d4eb9a3b3b91b7f18f7a7&DefaultTransportModeCode=BUS",{
+    const JourneyApi = async (stopArray: any) => {
+      // Getting CORS errors from trafiklab. "Mocking" instead.
+      /*const data = await fetch("https://api.sl.se/api2/LineData.json?model=jour&key=[key]&DefaultTransportModeCode=BUS",{
         method: "GET"
       });
       const jsonJourneyData = await data.json();*/
@@ -60,64 +26,84 @@ function App() {
       });
       const jsonJourneyData = await data.json();
 
-      // console.log('data', jsonJourneyData.ResponseData.Result);
-      // setJourneyData(jsonJourneyData.ResponseData.Result);
-      
       let lines: React.SetStateAction<topProps[]> = [];
       for (var i = 0; i < jsonJourneyData.ResponseData.Result.length; i++) {
         const key:number = +jsonJourneyData.ResponseData.Result[i]['LineNumber'];
+        const stopKey:number = +jsonJourneyData.ResponseData.Result[i]['JourneyPatternPointNumber'];
         if (!lines[key]) {
-          lines[key] = { count: 0, stops: [] }
+          lines[key] = { count: 0, line: 0, stops: [] }
         }
-        lines[key].count++;
-        lines[key].stops.push(jsonJourneyData.ResponseData.Result[i]['JourneyPatternPointNumber']); 
-      }
+        lines[key].line = key;
 
-      /* const result = Object
-        .keys(lines)
-        .sort((a, b) => lines[b].count - count[a].count)
-        [0];
-      */
-      // const result = getKeysWithHighestValue(lines, 10)
-      // console.log('result', result);      
+        // Only count unique stops that is present in stops list.
+        if (!lines[key].stops.includes(stopArray[stopKey]) ) {
+          lines[key].count++;
+          lines[key].stops.push(
+            stopArray[stopKey]
+          );
+        }
+      }
+      // `${stopArray[stopKey]} - ${stopKey}`
+
+      // Sort by count and get 10 most populated stops.
+      lines.sort((a, b) => b.count - a.count);
+      const topLines = lines.slice(0, 10);
+
+      settop10Lines(topLines);
     };
 
-    /* const StopsApi = async () => {
-      const data = await fetch("https://api.sl.se/api2/LineData.json?model=stop&key=b138ca24481d4eb9a3b3b91b7f18f7a7&DefaultTransportModeCode=BUS",{
+    const StopsApi = async () => {
+      /* const data = await fetch("https://api.sl.se/api2/LineData.json?model=stop&key=[key]&DefaultTransportModeCode=BUS",{
         method: "GET"
       });
+      const jsonStopsData = await data.json(); */
+      const data = await fetch("./trafiklab/stopBus.json",{
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      });
       const jsonStopsData = await data.json();
-      setStopsData(jsonStopsData.results);
-    }; */
+      const stopData = jsonStopsData.ResponseData.Result;
 
-     JourneyApi();
-     // StopsApi();
+      // Create the array with StopAreaNumber as key and StopPointName as value
+      const stopArray = stopData.reduce((arr: { [x: string]: any; }, stop: { StopPointNumber: string | number; StopPointName: any; }) => {
+        arr[stop.StopPointNumber] = stop.StopPointName;
+        return arr;
+      }, []);
+      
+      //Trigger journey API call with stop data.
+      JourneyApi(stopArray);
+    };
+    
+    StopsApi();
   }, []);
 
-  // Handle data and get top 10 stops.
-  // let top10Lines: never[] = [];
-  // const [top10Lines, settop10Lines] = useState<topProps[]>([]);
-  /*
-  let lines: React.SetStateAction<topProps[]> = [];
-  for (var i = 0; i < JourneyData.length; i++) {
-    const key = JourneyData[i]['LineNumber'];
-    lines[key]['count']++;
-    lines[key]['stops'][] = JourneyData[i]['JourneyPatternPointNumber']; 
-  }
-  console.log('top10Lines', top10Lines);
-  setJourneyData(lines);
-  */
+  // Added variable to keep track of open/close items.
+  const [openItems, setOpenItems] = useState<boolean[]>(Array(top10Lines.length).fill(false));
+  
+  const toggleOpen = (index: number) => {
+    const updatedOpenItems = [...openItems];
+    updatedOpenItems[index] = !updatedOpenItems[index];
+    setOpenItems(updatedOpenItems);
+  };
 
-  /*
-  JourneyData.map((value) => {
-    value.LineNumber
-  });
-  */
-
-  const listObject = {nameLabel:'titta', total: 2};
   return (
     <div className="App">
-      <Toplist list={listObject} />
+      <h1>Top 10 bus-stops with most stops</h1>
+      {top10Lines.map((item, index) => (
+        <div key={index}>
+          <button onClick={() => toggleOpen(index)}><h2>Line {item.line} with {item.count} stops</h2></button>
+          {openItems[index] && (
+          <ul>
+            <li className="stopsLabel"><strong>Stops:</strong></li>
+            {item.stops.map((stop: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined, stopIndex: React.Key | null | undefined) => (
+              <li key={stopIndex}>{stop}</li>
+            ))}
+          </ul>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
